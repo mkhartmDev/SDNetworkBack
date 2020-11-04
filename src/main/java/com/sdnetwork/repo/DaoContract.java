a5b3c131261743c008096374ebc6f7f9f854069a
KELSEY Iafrate <kiafrate13@gmail.com>
2020-11-04 16:11:52 -0600

    adds dao contract

---
+++ src/main/java/com/sdnetwork/repo/DaoContract.java
@@ -0,0 +1,18 @@
+package com.sdnetwork.repo;
+
+import java.util.List;
+
+public interface DaoContract <T, I> {
+
+	List<T> findAll();
+	
+	T findById(I i);
+	
+	T update(T t);
+	
+	T save(T t);
+	
+	T delete(I i);
+	
+	
+}
