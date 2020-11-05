3e84a389b78a38b43b0f12eaf982335758fdc482
KELSEY Iafrate <kiafrate13@gmail.com>
2020-11-04 20:55:01 -0600

    made the basics for the UserDao and wrote some tests in the Driver

---
+++ src/main/java/com/sdnetwork/repo/Driver.java
@@ -0,0 +1,21 @@
+package com.sdnetwork.repo;
+
+import com.sdnetwork.model.User;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		UserDao ud = new UserDao();
+		User u = new User("kels", "kels", "kels", "kels", "kels", null);
+		
+		System.out.println(ud.save(u));
+		System.out.println(ud.findAll());
+		System.out.println(ud.findById(1));
+		u.setLastName("iaf");
+		System.out.println(ud.update(u));
+		System.out.println(ud.delete(u.getUserId()));
+
+	}
+
+}
