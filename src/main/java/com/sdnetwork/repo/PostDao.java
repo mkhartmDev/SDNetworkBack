9486f9074f8c9108261344727de9218a9c27ba3d
unknown <brooke.wursten@gmail.com>
2020-11-04 19:54:54 -0700

    fixed annotations and added to Daos

--- src/main/java/com/sdnetwork/repo/PostDao.java
+++ src/main/java/com/sdnetwork/repo/PostDao.java
@@ -6,72 +6,69 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
-import com.sdnetwork.model.User;
+import com.sdnetwork.model.Post;
 import com.sdnetwork.util.HibernateUtil;
 
 @Repository
-public class UserDao implements DaoContract<User, Integer> {
-
+public class PostDao implements DaoContract<Post,Integer>{
+	
 	@Override
-	public List<User> findAll() {
-		List<User> list = HibernateUtil.getSessionFactory().openSession()
-				.createNativeQuery("select * from user", User.class).list();
+	public List<Post> findAll() {
+		List<Post> list = HibernateUtil.getSessionFactory().openSession()
+				.createNativeQuery("select * from post", Post.class).list();
 		return list;
 	}
 
 	@Override
-	public User findById(Integer i) {
+	public Post findById(Integer i) {
 		Session sess = HibernateUtil.getSessionFactory().openSession();
-		User u = sess.get(User.class, i);
-		return u;
+		Post p = sess.get(Post.class, i);
+		return p;
 	}
 
 	@Override
-	public User update(User u) {
+	public Post update(Post p) {
 		Session sess;
 		Transaction tx=null;
 		try {
 		sess = HibernateUtil.getSessionFactory().openSession();
 		tx = sess.beginTransaction();
-		sess.update(u);
+		sess.update(p);
 		tx.commit();
 		}catch(Exception e) {
 			e.printStackTrace();
 			tx.rollback();
 		}
-		return u;
+		return p;
 	}
 
 	@Override
-	public User save(User u) {
+	public Post save(Post p) {
 		Session sess;
 		Transaction tx=null;
 		try {
 		sess = HibernateUtil.getSessionFactory().openSession();
 		tx = sess.beginTransaction();
-		sess.persist(u);
+		sess.persist(p);
 		tx.commit();
 		}catch(Exception e) {
 			e.printStackTrace();
 			tx.rollback();
 		}
-		return u;
+		return p;
 	}
 
 	@Override
-	public User delete(Integer i) {
+	public Post delete(Integer i) {
 		Session sess = HibernateUtil.getSessionFactory().openSession();
-		User u = sess.get(User.class, i);
-		sess.delete(u);
-		return u;
+		Post p = sess.get(Post.class, i);
+		sess.delete(p);
+		return p;
 	}
 	
-	public User findByUsername(String username) {
+	public List<Post> findByUsername(String username) {
 		Session sess = HibernateUtil.getSessionFactory().openSession();
-		return sess.createQuery("from user where name = '"+username+"'", User.class).list().get(0);
+		return sess.createQuery("from user where name = '"+username+"'", Post.class).list();
 	}
-	
-	
-	
 
 }
