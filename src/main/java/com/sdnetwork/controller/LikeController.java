31e9cfffa474ec6dc55051cd1f4f41b435d7e35c
KELSEY Iafrate <kiafrate13@gmail.com>
2020-11-11 16:23:06 -0600

    removed unused imports

--- src/main/java/com/sdnetwork/controller/LikeController.java
+++ src/main/java/com/sdnetwork/controller/LikeController.java
@@ -1,15 +1,11 @@
 package com.sdnetwork.controller;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sdnetwork.dto.RestLike;
