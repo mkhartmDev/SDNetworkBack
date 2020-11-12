63a110d2150a4da96da8f926f521ef973b0473d6
KELSEY Iafrate <kiafrate13@gmail.com>
2020-11-12 14:55:31 -0600

    writing some tests in UserDao

---
+++ src/test/java/com/sdnetwork/repoTest/PostDaoTest.java
@@ -0,0 +1,49 @@
+package com.sdnetwork.repoTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PostDaoTest {
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddLike() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveLike() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/test/java/com/sdnetwork/repoTest/UserDaoTest.java
+++ src/test/java/com/sdnetwork/repoTest/UserDaoTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,44 +27,54 @@ class UserDaoTest {
 	private static ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:applicationContext-test.xml");
 	
 	private UserDao ud = ac.getBean(UserDao.class);
+	private User user0 = new User(0,"kelsey","kelsey","kelsey","kelsey","kelsey","");
+	
+	@Test
+	void testASave() {
+		assertNotNull(ud.save(user0));
+		assertNull(ud.save(user0));
+		fail("Not yet implemented");
+	}
 
 	@Test
 	@Transactional
-	void testFindAll() {
+	void testBFindAll() {
 		List<RestUser> users = ud.findAll();
 		System.out.println(users);
 		assertEquals(1, users.size());
 	}
 
 	@Test
-	void testFindById() {
+	void testCFindById() {
+		assertNotNull(ud.findById(user0.getUserId()));
+		assertNull(ud.findById(2));
 		fail("Not yet implemented");
 	}
 
+	@Test
+	void testDFindByUsername() {
+		assertNotNull(ud.findByUsername(user0.getUsername()));
+		assertNull(ud.findByUsername("yeslek"));
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testEFindByEmail() {
+		fail("Not yet implemented");
+	}
+	
 	@Test
 	void testUpdate() {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	void testSave() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testDelete() {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	void testFindByUsername() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	void testFindByEmail() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testFindBySearch() {
